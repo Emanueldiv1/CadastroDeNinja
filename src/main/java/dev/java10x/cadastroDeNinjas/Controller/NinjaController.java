4ca87e2bee987ce9368996cb85cef30bf86b0d1c
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-06-27 20:26:26 -0300

    criando entidade ninja no BD

--- src/main/java/dev/java10x/cadastroDeNinjas/Controller/NinjaController.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Controller/NinjaController.java
@@ -1,4 +1,4 @@
-package dev.java10x.cadastroDeNinjas;
+package dev.java10x.cadastroDeNinjas.Controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/ninja")
-public class Controller {
+public class NinjaController {
 
 
     @GetMapping
