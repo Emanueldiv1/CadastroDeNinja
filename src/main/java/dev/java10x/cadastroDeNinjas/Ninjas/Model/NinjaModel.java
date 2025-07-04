52d32bd1a49c54b45c6ad001c9d7e22525777405
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-07-04 20:37:26 -0300

    feat: Coloca o Lombook na classe para abstração

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/NinjaModel.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Model/NinjaModel.java
@@ -2,6 +2,9 @@ package dev.java10x.cadastroDeNinjas.Ninjas.Model;
 
 import dev.java10x.cadastroDeNinjas.Missoes.Model.MissoesModel;
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
@@ -9,6 +12,9 @@ import java.util.List;
 
 @Entity
 @Table(name = "TB_CADASTRO")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data // |Lombook/@Data|  Criar todos os get/set
 public class NinjaModel {
 
    @Id
@@ -26,38 +32,5 @@ public class NinjaModel {
    @JoinColumn(name = "missoes_id") // chave estrangeira
    private MissoesModel missoes;
 
-    public NinjaModel() {
-    }
 
-
-
-    public NinjaModel(String name, String email, int idade) {
-        this.name = name;
-        this.email = email;
-        this.idade = idade;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public int getIdade() {
-        return idade;
-    }
-
-    public void setIdade(int idade) {
-        this.idade = idade;
-    }
 }
