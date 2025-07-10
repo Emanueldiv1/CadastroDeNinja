c205ea81c8f8a2fe19e325e26804d1af4da54bae
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-07-10 19:12:43 -0300

    chore: Organizar estrutura pastas e adicionar nova classe

---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Repository/MissoesRepository.java
@@ -0,0 +1,4 @@
+package dev.java10x.cadastroDeNinjas.Missoes.Repository;
+
+public class MissoesRepository {
+}
---
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Service/MissoesService.java
@@ -0,0 +1,5 @@
+package dev.java10x.cadastroDeNinjas.Missoes.Service;
+
+public class MissoesService {
+
+}
