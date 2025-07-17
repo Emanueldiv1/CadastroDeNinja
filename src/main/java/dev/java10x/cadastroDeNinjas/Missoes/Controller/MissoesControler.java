3eb37b6b7121605c89eca69618b4a5f3cf96a8b3
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-07-17 19:39:56 -0300

    feat: Cria rota do Controller com mock

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Controller/MissoesControler.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Controller/MissoesControler.java
@@ -1,4 +1,46 @@
 package dev.java10x.cadastroDeNinjas.Missoes.Controller;
 
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/missoes")
 public class MissoesControler {
+
+    //Add missoes
+    //Post -- Mandar uma requsição para crias as missoes
+    @PostMapping("/add")
+    public String criarMissao(){
+        return "missão criada";
+    }
+
+
+    //Mostrar td os missoes
+    //Get -- Mandar uma requsição para mostrar as missoes
+    @GetMapping("/allmissoes")
+    public String todasMissoes(){
+        return "Todos missoes";
+    }
+
+
+    //Mostrar missoes por ID
+    //Get -- Mandar uma requsição para mostrar as missoes por ID
+    @GetMapping("/missoesID")
+    public String todasMissoesPorID(){
+        return "missoes por ID";
+    }
+
+    //Alterar dados missoess
+    //Put -- Mandar uma requsição para alterar as Missoes
+    @PutMapping("/alterarmissoes")
+    public String alterarMissoes(){
+        return "missoes alterado";
+    }
+
+    //Deletar missoes
+    //Delete -- Mandar uma requsição para deletar as missoes
+    @DeleteMapping("/deletarmissaoID")
+    public String deletarMissaoID(){
+        return "MISSÃO EXCLUIDO";
+    }
 }
