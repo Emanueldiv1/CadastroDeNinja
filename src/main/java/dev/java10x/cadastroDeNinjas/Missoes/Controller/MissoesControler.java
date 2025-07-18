578d9ebaca73d201587e7ccbd7d84f947faf167a
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-07-18 20:34:32 -0300

    feat: adiciona serviço de listagem de missoes

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Controller/MissoesControler.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Controller/MissoesControler.java
@@ -1,11 +1,23 @@
 package dev.java10x.cadastroDeNinjas.Missoes.Controller;
 
 
+import dev.java10x.cadastroDeNinjas.Missoes.Model.MissoesModel;
+import dev.java10x.cadastroDeNinjas.Missoes.Service.MissoesService;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/missoes")
 public class MissoesControler {
+
+    public MissoesService missoesService;
+
+    public  MissoesControler(MissoesService missoesService){
+        this.missoesService = missoesService;
+    }
+
+    //--------------------------------------------------------------//-----------------------------------
 
     //Add missoes
     //Post -- Mandar uma requsição para crias as missoes
@@ -18,8 +30,8 @@ public class MissoesControler {
     //Mostrar td os missoes
     //Get -- Mandar uma requsição para mostrar as missoes
     @GetMapping("/allmissoes")
-    public String todasMissoes(){
-        return "Todos missoes";
+    public List<MissoesModel>todasMissoes(){
+       return missoesService.listarMissoes();
     }
 
 
@@ -29,6 +41,7 @@ public class MissoesControler {
     public String todasMissoesPorID(){
         return "missoes por ID";
     }
+
 
     //Alterar dados missoess
     //Put -- Mandar uma requsição para alterar as Missoes
--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Service/MissoesService.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Service/MissoesService.java
@@ -1,5 +1,25 @@
 package dev.java10x.cadastroDeNinjas.Missoes.Service;
 
+import dev.java10x.cadastroDeNinjas.Missoes.Model.MissoesModel;
+import dev.java10x.cadastroDeNinjas.Missoes.Repository.MissoesRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class MissoesService {
+
+    public MissoesRepository missoesRepository;
+
+
+    public MissoesService(MissoesRepository missoesRepository) {
+        this.missoesRepository = missoesRepository;
+    }
+
+    public List<MissoesModel> listarMissoes(){
+        return missoesRepository.findAll();
+    }
+
+
 
 }
