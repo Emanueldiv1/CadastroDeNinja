e372a717844065ff5060e125dc571e66cd3b4117
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-07-24 22:17:24 -0300

    chore: inclui a versão do DTO pela entidade e mapeia manualmente no NinjaMapper os caminhos entidade x DTO

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/Mapper/NinjaMapper.java
@@ -1,4 +1,35 @@
 package dev.java10x.cadastroDeNinjas.Ninjas.Mapper;
 
+import dev.java10x.cadastroDeNinjas.Ninjas.Dto.NinjaDTO;
+import dev.java10x.cadastroDeNinjas.Ninjas.Model.NinjaModel;
+
 public class NinjaMapper {
+    public NinjaModel map(NinjaDTO ninjaDTO){
+        NinjaModel ninjaModel = new NinjaModel();
+
+        ninjaModel.setId(ninjaDTO.getId());
+        ninjaModel.setName(ninjaDTO.getName());
+        ninjaModel.setEmail(ninjaDTO.getEmail());
+        ninjaModel.setImgUrl(ninjaDTO.getImgUrl());
+        ninjaModel.setIdade(ninjaDTO.getIdade());
+        ninjaModel.setRank(ninjaDTO.getRank());
+        ninjaModel.setMissoes(ninjaDTO.getMissoes());
+
+        return ninjaModel;
+    }
+
+    public NinjaDTO map(NinjaModel ninjaModel){
+        NinjaDTO ninjaDTO = new NinjaDTO();
+
+        ninjaDTO.setId(ninjaModel.getId());
+        ninjaDTO.setName(ninjaModel.getName());
+        ninjaDTO.setEmail(ninjaModel.getEmail());
+        ninjaDTO.setImgUrl(ninjaModel.getImgUrl());
+        ninjaDTO.setIdade(ninjaModel.getIdade());
+        ninjaDTO.setRank(ninjaModel.getRank());
+        ninjaDTO.setMissoes(ninjaModel.getMissoes());
+
+        return ninjaDTO;
+
+    }
 }
