238472734f21d9e373beae8c1949b6b791bc5d54
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-08-01 23:01:59 -0300

    feat: adiciona estrutura DTO e Mapper

--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/DTO/MissoesDTO.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/DTO/MissoesDTO.java
@@ -1,4 +1,24 @@
 package dev.java10x.cadastroDeNinjas.Missoes.DTO;
 
+import dev.java10x.cadastroDeNinjas.Ninjas.Dto.NinjaDTO;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class MissoesDTO {
+
+    private Long id;
+
+    private String nome;
+
+    private String dificuldade;
+
+    private String descricao;
+
+    private List<NinjaDTO> ninja;
 }
--- src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Missoes/Mapper/MissoesMapper.java
@@ -1,4 +1,32 @@
 package dev.java10x.cadastroDeNinjas.Missoes.Mapper;
 
+import dev.java10x.cadastroDeNinjas.Missoes.DTO.MissoesDTO;
+import dev.java10x.cadastroDeNinjas.Missoes.Model.MissoesModel;
+import dev.java10x.cadastroDeNinjas.Ninjas.Dto.NinjaDTO;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MissoesMapper {
+
+    public  MissoesModel map(MissoesDTO missoesDTO){
+        MissoesModel missoesModel = new MissoesModel();
+
+        missoesModel.setId(missoesDTO.getId());
+        missoesModel.setNome(missoesDTO.getNome());
+        missoesModel.setDificuldade(missoesDTO.getDificuldade());
+        missoesModel.setDescricao(missoesDTO.getDescricao());
+
+        return missoesModel;
+    }
+
+    public MissoesDTO map(MissoesModel missoesModel){
+        MissoesDTO missoesDTO = new MissoesDTO();
+
+        missoesDTO.setId(missoesModel.getId());
+        missoesDTO.setNome(missoesModel.getNome());
+        missoesDTO.setDificuldade(missoesModel.getDificuldade());
+        missoesDTO.setDescricao(missoesModel.getDescricao());
+
+        return missoesDTO;
+    }
 }
