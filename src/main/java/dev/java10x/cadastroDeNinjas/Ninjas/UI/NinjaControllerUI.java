ac4cc9c6a5bfe13d564632c9d3f43fa5dcc56ccf
Emanuel Rodrigues <fergimanu3@gmail.com>
2025-08-12 19:36:44 -0300

    feat: adicionado rota detalhes do ninja para uma nova pag html

--- src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/UI/NinjaControllerUI.java
+++ src/main/java/dev/java10x/cadastroDeNinjas/Ninjas/UI/NinjaControllerUI.java
@@ -2,10 +2,13 @@ package dev.java10x.cadastroDeNinjas.Ninjas.UI;
 
 import dev.java10x.cadastroDeNinjas.Ninjas.Dto.NinjaDTO;
 import dev.java10x.cadastroDeNinjas.Ninjas.Service.NinjaService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
@@ -25,6 +28,26 @@ public class NinjaControllerUI {
         List<NinjaDTO> ninjas = ninjaService.listarNinjas();
         model.addAttribute("ninjas", ninjas);
         return "listarNinjas"; // tem que retornar o nome da pag que renderiza
+    }
+
+    @GetMapping("/deletar/{id}")
+    public String deletarNinjaID(@PathVariable Long id){
+       ninjaService.deletarNinja(id);
+       return "redirect:/ninjas/ui/listar";
+    }
+
+    @GetMapping("/listar/{id}")
+    public String NinjasPorID(@PathVariable Long id, Model model){
+        NinjaDTO ninja = ninjaService.listarNinjaId(id);
+
+        if (ninja != null){
+            model.addAttribute("ninjas", ninja);
+            return "detalhesNinja";
+        }
+        else{
+            model.addAttribute("Mensagem", "Ninja não encontrado" );
+            return "listarNinjas";
+        }
     }
 
 }
